dfb28a763d8eabaf0f17e75f850ed96b04e287c4
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-05-30 22:53:20 +0300

    updated

--- src/test/java/techproed/day03_Locators/C01_WebelementLocators.java
+++ src/test/java/techproed/day03_Locators/C01_WebelementLocators.java
@@ -1,8 +1,8 @@
 package techproed.day03_Locators;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -19,13 +19,19 @@ public class C01_WebelementLocators {
         myDriver.get("https://www.amazon.com");
 
         //Arama kutusunu locate edelim.
-        myDriver.findElement(By.id("twotabsearchtextbox"));
+        //Arama kutusuna iphone yazdıralım ve aratalım.
+        myDriver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
         /*
-        Bir webelementin locate'ini birden fazla kullanacaksanız bir Webelement'e değişken olarak atayabilirsiniz.
-        Webelement aramaKutusu = myDriver.findElement(By.id("twotabsearchtextbox")); şeklinde kullanabiliriz.
+            1--> Bir webelementin locate'ini birden fazla kullanacaksanız bir Webelement'e değişken olarak atayabilirsiniz.
+            Webelement aramaKutusu = myDriver.findElement(By.id("twotabsearchtextbox")); şeklinde kullanabiliriz.
+            2--> Locate işlemi bittikten sonra eğer webelemente metin göndereceksek sendKeys() methodu kullanırız.
+            3--> Webelemente tıklayacaksak click() methodu kullanırız.
+            4--> Webelementin üzerindeki yazıyı almak istiyorsak getText() kullanırız.
+            5--> sendKeys() methodundan sonra(yani web elemente metin gönderdikten sonra) Keys.ENTER ile ya da
+                 submit() methodu ile manuel olarak enter yaptığımız gibi otomasyonda da yapabiliriz.
          */
 
-        //Arama kutusuna iphone yazdıralım ve aratalım.
         //Sayfayı kapatalım.
+        myDriver.close();
     }
 }
---
+++ src/test/java/techproed/day03_Locators/C02_Submit.java
@@ -0,0 +1,32 @@
+package techproed.day03_Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.security.Key;
+import java.time.Duration;
+
+public class C02_Submit {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/resources/drivers/chromedriver.exe");
+        WebDriver myDriver = new ChromeDriver();
+        myDriver.manage().window().maximize();
+        myDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //Amazon sayfasına gidelim.
+        //Arama kutusunu locate edip iphone aratalım.
+        myDriver.get("https:www.amazon.com");
+        WebElement aramaKutusu = myDriver.findElement(By.name("field-keywords"));
+        aramaKutusu.sendKeys("iphone");
+        Thread.sleep(5000);
+        aramaKutusu.submit();//Keys.ENTER
+
+
+        //Sayfayı kapatalım.
+        myDriver.close();
+
+    }
+}
